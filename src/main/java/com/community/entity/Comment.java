44873d3f115f1fcd8240ab5a6c42292ff9a31359
Gnawix <3240075979@qq.com>
2022-06-15 00:10:42 +0800

    评论模块

---
+++ src/main/java/com/community/entity/Comment.java
@@ -0,0 +1,99 @@
+package com.community.entity;
+
+import java.util.Date;
+
+public class Comment {
+
+    private int id;
+    // 发评论的用户
+    private int userId;
+    // 被评论的目标类型 1是对帖子的评论 2是对评论的评论
+    private int entityType;
+    // 被评论的帖子id
+    private int entityId;
+    // 帖子下互相评论的目标评论id
+    private int targetId;
+    private String content;
+    // 状态 1正常 0删除
+    private int status;
+    private Date createTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(int entityType) {
+        this.entityType = entityType;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public int getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(int targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", entityType=" + entityType +
+                ", entityId=" + entityId +
+                ", targetId=" + targetId +
+                ", content='" + content + '\'' +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
+
